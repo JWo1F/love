3307e6c22fffcb18f74974a93110ebfb6662fe78
WolF™ <J-Wo1F@yandex.ru>
2013-07-25 15:35:10 -0400

    add block page

--- public/core.js
+++ public/core.js
@@ -1,47 +1,60 @@
-var size = view.size.height/1.5;
+var $ = function (id) {return document.getElementById(id)};
+var date = new Date($('date').value);
+      
+function setDate(b,c,a,e){var d=parseFloat(b.toString()[b.toString().length-1]);if(b>10&&b<21||!d){return e}if(d==1){return c}else{if(d>1&&d<5){return a}else{if(d>4&&d<11){return e}}}};
 
-var heart = new Path.Circle(view.center, size/2);
-heart.segments[1].point.y += size/3.5;
-heart.segments[1].handleIn.y -= size/3.5;
-heart.segments[1].handleOut.y -= size/3.5;
-console.log(heart.segments[1].handleIn);
-heart.segments[3].point.y += size/7;
-heart.segments[3].handleIn.y -= size/7;
-heart.segments[3].handleOut.y -= size/7;
-heart.segments[0].point.x -= size/15;
-heart.segments[2].point.x += size/15;
+setInterval(function () {
+  var curr = new Date();
+  var diff = curr - date;
+  
+  var sec = Math.floor(diff/1000 % 60);
+  var min = Math.floor(diff/1000/60 % 60);
+  var hou = Math.floor(diff/1000/60/60 % 24);
+  var day = Math.abs(curr.getDate() - date.getDate());
+  var mon = Math.abs(curr.getMonth() - date.getMonth());
+  var yea = curr.getFullYear() - date.getFullYear();
+  
+  
+  $('yea').innerHTML = yea + ' ' + setDate(yea, 'год', 'года', 'лет') + ', ';
+  $('mon').innerHTML = mon + ' ' + setDate(mon, 'месяц', 'месяца', 'месяцев') + ', ';
+  $('day').innerHTML = day + ' ' + setDate(day, 'день', 'дня', 'дней') + ', ';
+  $('hou').innerHTML = hou + ' ' + setDate(hou, 'час', 'часа', 'часов') + ', ';
+  $('min').innerHTML = min + ' ' + setDate(min, 'минуту', 'минуты', 'минут') + ', ';
+  $('sec').innerHTML = sec + ' ' + setDate(sec, 'секунду', 'секунды', 'секунд');
+}, 1000);
 
-//heart.fullySelected = true;
-heart.fillColor = {
-  gradient: {
-    stops: [['#ff3600', 0], ['#a31b01', 1]],
-    radial: true
-  },
-  origin: view.center,
-  destination: heart.segments[2].point
-};
-heart.strokeColor = 'black';
+var opacity = 1;
+var block = $('block').style;
 
-heart.translate(new Point(0, -size/7));
-
-var stage = 0;
-var flag = false;
-
-function onFrame () {
-  if (stage < 10) {
-    heart.scale(0.97);
-    stage++;
-  } else if ((stage < 50 && flag) || (stage < 20 && !flag)) {
-    stage++;
-  } else if ((stage < 60 && flag) || (stage < 30 && !flag)) {
-    heart.scale(1.0309);
-    stage++;
-  } else {
-    stage = 0;
-    flag = (flag) ? false : true;
-  }
+if (localStorage.read) block.display = 'none';
+else {
+  var lost = localStorage.lost || 1000 * 60 * 5;
+  
+  var timer = setInterval(function () {
+    var seconds = Math.floor(lost/1000 % 60);
+    var minutes = Math.floor(lost/1000/60 % 60);
+    if (seconds.toString().length == 1) seconds = '0' + seconds;
+    if (minutes.toString().length == 1) minutes = '0' + minutes;
+    $('timer').innerHTML = minutes + ':' + seconds;
+    lost -= 1000;
+    localStorage.lost = lost;
+    if (lost <= 0) {
+      clearInterval(timer);
+      localStorage.read = true;
+      localStorage.removeItem('lost');
+      timer = setInterval(function () {
+        block.opacity = opacity;
+        opacity -= 0.02;
+        if (!opacity) {
+          clearInterval(timer);
+          block.display = 'none';
+        }
+      }, 1000/40);
+    }
+  }, 1000);
 }
 
-var textItem = new PointText(new Point(20, window.innerHeight - 20));
-textItem.fillColor = '#ccc';
-textItem.content = 'Design by JWo1F';
+$('skip').onclick = function () {
+  localStorage.read = true;
+  location.reload();
+}
